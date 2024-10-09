76d88f73194dcd4aeddf438cd77dfee376c5f0c9
Shourya Bose <sbose@gpu5.lcrc.anl.gov>
2024-10-09 08:37:25 -0500

    pytorch finetuning

--- notebooks/train_comstock.py
+++ notebooks/train_comstock.py
@@ -18,13 +18,16 @@ plt.rcParams['text.usetex'] = True
 plt.rcParams.update({'font.size': 14})
 plt.rcParams['text.latex.preamble'] = r'\usepackage{times}'
 
-device = torch.device('cuda:0')
+device, device_2 = torch.device('cuda:0'), torch.device('cuda:1')
 
 # batch size
-batch_size = 1024
+batch_size = 512
 
 # lookback and lookahead
 lookback, lookahead = 96, 96
+
+# number of buildings
+num_bldg = 12
 
 # data
 tcm = TFMbase.TimesFmHparams
@@ -38,7 +41,7 @@ dataset_train, dataset_val, dataset_test, mean, std = TFMdset.get_data_and_gener
     data_array = data,
     split_ratios = [0.8,0.1,0.1],
     dataset_kwargs = {
-        'num_bldg' : data.shape[0],
+        'num_bldg' : num_bldg,
         'lookback' : lookback,
         'lookahead' : lookahead,
         'normalize' : True,
@@ -62,14 +65,16 @@ if __name__ == "__main__":
     optim = torch.optim.Adam(model.parameters(),lr=1e-6)
     steps, epoch = 0, 0
     save_every = 100
-    epochs = 100
+    epochs = 20
     train_len, test_len = len(dl), len(dl_test)
 
     for epoch in range(epochs):
+        # train
+        model = model.to(device) # move to primary cuda
         for itm in (t:=tqdm(dl)):
             start = time.time()
-            ts, paddings, freq, y, x_unpadded = itm
-            ts, paddings, freq, y, x_unpadded = ts.to(device), paddings.to(device), freq.to(device), y.to(device), x_unpadded.to(device)
+            ts, paddings, freq, y, _ = itm
+            ts, paddings, freq, y = ts.to(device), paddings.to(device), freq.to(device), y.to(device)
             out, _ = model.decode(
                 ts,
                 paddings,
@@ -90,24 +95,25 @@ if __name__ == "__main__":
             if (steps % save_every == 0) or (steps==train_len):
                 losses_train.to_csv('train.csv')
             end = time.time()
-            t.set_description(f"Step {steps+1} on epoch {epoch+1} took {(end-start):.3f}s. MSE loss: {mse_loss:.5f}, MAE loss: {mae_loss:.5f}. Expected time for this epoch: {int((end-start)*train_len)}s")
+            print(f"Step {steps+1} on epoch {epoch+1} took {(end-start):.3f}s. MSE loss: {mse_loss:.5f}, MAE loss: {mae_loss:.5f}. Expected time for this epoch: {int((end-start)*train_len)}s")
         # test
+        model = model.to(device_2) # move to secondary cuda
         test_mse, test_mae = 0. , 0.
         for itm in dl_test:
-            ts, paddings, freq, y, x_unpadded = itm
-            ts, paddings, freq, y, x_unpadded = ts.to(device), paddings.to(device), freq.to(device), y.to(device), x_unpadded.to(device)
+            ts, paddings, freq, y, _ = itm
+            ts, paddings, freq, y = ts.to(device_2), paddings.to(device_2), freq.to(device_2), y.to(device_2)
             out, _ = model.decode(
                 ts,
                 paddings,
                 freq,
                 horizon_len = lookahead
             )
-            test_mse += F.mse_loss(out,y,reduction='mean')
-            test_mae += F.l1_loss(out,y,reduction='mean')
+            test_mse += F.mse_loss(out,y,reduction='mean').item()
+            test_mae += F.l1_loss(out,y,reduction='mean').item()
         if epoch == 0:
             losses_test = pd.DataFrame({'mse_loss':[test_mse/test_len],'mae_loss':[test_mae/test_len]})
             losses_test.index.name = 'epoch'
         else:
-            losses_test.loc[epoch].append({'mse_loss':test_mse/test_len,'mae_loss':test_mae/test_len})
+            losses_test.loc[epoch] = {'mse_loss':test_mse/test_len,'mae_loss':test_mae/test_len}
         losses_test.to_csv('test.csv')
         torch.save(model.state_dict(),'model.pth')        
